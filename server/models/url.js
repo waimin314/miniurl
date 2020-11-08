a8b2a7bb3c7ee56365a7ccb8b345e766b374fd95
Wai Min <leopard23@gmail.com>
2020-11-08 12:46:56 +0800

    Create model for Url

---
+++ server/models/url.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const urlSchema = mongoose.Schema({
+  miniurl: { type: String, required: true, minlength: 7 },
+  fullurl: { type: String, required: true, minlength: 10 },
+  slug: { type: String, required: true, length: 7 },
+});
+
+urlSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Url', urlSchema);
