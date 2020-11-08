8c4efc2cc19027f420db7b2fa73c660d1c96f0bf
Wai Min <leopard23@gmail.com>
2020-11-08 20:23:06 +0800

    Check if url is valid before saving to MongoDB
    Check URL format is correct with 'valid-url' package before saving to DB. Send status 400 if not valid. Added sample POST request for sending invalid url in API_samples.rest

--- server/controllers/urls.js
+++ server/controllers/urls.js
@@ -1,5 +1,6 @@
 const urlsRouter = require('express').Router();
 const { nanoid } = require('nanoid');
+const validUrl = require('valid-url');
 const URL = require('../models/url');
 const { HOMEPAGE_URL } = require('../utils/config');
 
@@ -7,6 +8,11 @@ const MAX_SLUG_LENGTH = 7;
 
 urlsRouter.post('/', async (request, response) => {
   const body = request.body;
+
+  if (!validUrl.isWebUri(body.fullurl)) {
+    return response.status(400).send({ error: 'invalid link' });
+  }
+
   const slug = nanoid(MAX_SLUG_LENGTH);
   const url = new URL({
     miniurl: HOMEPAGE_URL + slug,
