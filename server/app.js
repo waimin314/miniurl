fe70b81781b1c92f3b5f5936f76a41b000247fd0
Wai Min <leopard23@gmail.com>
2020-11-08 12:25:12 +0800

    Implement skeleton code with proper file structure

---
+++ server/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+require('express-async-errors');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const middleware = require('./utils/middleware');
+const logger = require('./utils/logger');
+const config = require('./utils/config');
+
+const app = express();
+
+logger.info('connecting to', config.MONGODB_URI);
+
+mongoose
+  .connect(config.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    logger.info('connected to mongoDB');
+  })
+  .catch((err) => {
+    logger.error('error connecting to mongoDB: ', err.message);
+  });
+
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,1 +1,10 @@
-console.log('Hello World');
+const app = require('./app');
+const http = require('http');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+
+const server = http.createServer(app);
+
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
+});
