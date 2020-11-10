912d613052da3995874d231d0d4b0614a658bcd2
Wai Min <leopard23@gmail.com>
2020-11-10 21:38:30 +0800

    Implement GET method
    - Redirect valid links
    - Returns status 404 for invalid links

--- server/controllers/urls.js
+++ server/controllers/urls.js
@@ -1,10 +1,20 @@
 const urlsRouter = require('express').Router();
 const { nanoid } = require('nanoid');
 const validUrl = require('valid-url');
+const url = require('../models/url');
 const URL = require('../models/url');
 const { HOMEPAGE_URL } = require('../utils/config');
 
 const MAX_SLUG_LENGTH = 7;
+
+urlsRouter.get('/:slug', async (request, response) => {
+  const slug = request.params.slug;
+  const urlFromDB = await URL.findOne({ slug: slug });
+
+  if (!urlFromDB) return response.status(404).send();
+
+  response.redirect(urlFromDB.fullUrl);
+});
 
 urlsRouter.post('/', async (request, response) => {
   const body = request.body;
