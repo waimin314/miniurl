4e567528fa56cb890beac60585fafccac0a338f2
Wai Min <leopard23@gmail.com>
2020-11-14 21:57:47 +0800

    Add RedirectPage component
    - Create a component that shows a spinner and the page being redirected
    - Get the slug (pathname) of the Url and pass it to RedirectPage

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import RedirectPage from './components/RedirectPage';
 import UrlFrom from './components/UrlForm';
 
 function App() {
+  // Get pathname without '/' character
+  const slug = window.location.pathname.substring(1);
   return (
-    <div>
+    <div className='h-screen flex flex-col justify-center items-center'>
+      {slug.length > 0 && <RedirectPage slug={slug} />}
       <UrlFrom />
     </div>
   );
