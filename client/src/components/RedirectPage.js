b743a2cfdd79cc3d85667a86f513a3a5df63c2a2
Wai Min <leopard23@gmail.com>
2020-11-23 07:12:31 +0800

    Refactor UrlForm
    - Move the common info elements for Retrieving and Redirecting

--- client/src/components/RedirectPage.js
+++ client/src/components/RedirectPage.js
@@ -12,7 +12,7 @@ export default function RedirectPage({ slug }) {
     fetchUrl();
   }, [slug]);
 
-  const renderRetrieving = () => {
+  const infoCard = (message, url) => {
     return (
       <div
         className='bg-white p-5 rounded-md shadow-md 
@@ -40,10 +40,17 @@ export default function RedirectPage({ slug }) {
               d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
             ></path>
           </svg>
-          Retrieving full link from server for
+          {message}
         </div>
-        <p className='font-bold break-all'>{window.location.href}</p>
+        <p className='font-bold break-all'>{url}</p>
       </div>
+    );
+  };
+
+  const renderRetrieving = () => {
+    return infoCard(
+      'Retrieving full link from server for',
+      window.location.href
     );
   };
 
@@ -52,38 +59,7 @@ export default function RedirectPage({ slug }) {
 
     window.location.href = data.fullUrl;
 
-    return (
-      <div
-        className='bg-white p-5 rounded-md shadow-md 
-                      lg:w-3/5 lg:mx-auto'
-      >
-        <div className='flex items-center mb-5'>
-          {/* Spinning circle */}
-          <svg
-            className='animate-spin mr-3 h-6 w-6 text-indigo-600'
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 24 24'
-          >
-            <circle
-              className='opacity-25'
-              cx='12'
-              cy='12'
-              r='10'
-              stroke='currentColor'
-              strokeWidth='4'
-            ></circle>
-            <path
-              className='opacity-75'
-              fill='currentColor'
-              d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-            ></path>
-          </svg>
-          Redirecting to
-        </div>
-        <p className='font-bold break-all'>{data.fullUrl}</p>
-      </div>
-    );
+    return infoCard('Redirecting to', data.fullUrl);
   };
 
   const renderErrorMesage = () => {
